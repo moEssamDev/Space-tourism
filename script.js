2803211b492c5c0a8a6297071a75fad76fc5075e
moEssamDev <159295610+moEssamDev@users.noreply.github.com>
2024-04-07 01:55:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
 let menu_list_icon = document.getElementById("toggle");
 let menu_list = document.querySelector("header nav ul");
 
-menu_list_icon.onclick = function () {
-  menu_list.classList.toggle("openandclose");
+if (menu_list_icon) {
+  menu_list_icon.onclick = function () {
+    menu_list.classList.toggle("openandclose");
 
-  if (menu_list.classList.contains("openandclose")) {
-    menu_list_icon.src = "../../assets/shared/icon-close.svg";
-    if (menu_list.classList.contains("home")) {
-      menu_list_icon.src = "assets/shared/icon-close.svg";
+    if (menu_list.classList.contains("openandclose")) {
+      menu_list_icon.src = "../../assets/shared/icon-close.svg";
+    } else {
+      menu_list_icon.src = "../../assets/shared/icon-hamburger.svg";
     }
-  } else {
-    menu_list_icon.src = "../../assets/shared/icon-hamburger.svg";
-    if (menu_list.classList.contains("home")) {
-      menu_list_icon.src = "assets/shared/icon-hamburger.svg";
-    }
-  }
-};
-
-if (menu_list.classList.contains("openandclose")) {
-  if (menu_list_icon.classList.contains("home")) {
-    menu_list_icon.src = "assets/shared/icon-close.svg";
-  }
+  };
 }
+
+if (document.getElementById("hamburger")) {
+  document.getElementById("hamburger").onclick = function () {
+    document.getElementById("hamburger").classList.toggle("open");
+
+    if (document.getElementById("hamburger").classList.contains("open")) {
+      document.getElementById("hamburger").src = "assets/shared/icon-close.svg";
+      document.querySelector("header nav ul").style.right = 0;
+    } else {
+      document.getElementById("hamburger").src =
+        "assets/shared/icon-hamburger.svg";
+      document.querySelector("header nav ul").style.right = -350 + "px";
+    }
+  };
+}
+console.log(document.getElementById("hamburger"));
 
 // pages's transport transition
 document.querySelectorAll(".transition-link").forEach((link) => {
