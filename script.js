82debab5b070ac32cbc8022f580b9ef5dd2c15f1
mohamedEssam2004 <esqammed2004@gmail.com>
2024-04-06 14:39:25 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+let menu_list_icon = document.getElementById("toggle");
+let menu_list = document.querySelector("header nav ul");
+
+menu_list_icon.onclick = function () {
+  menu_list.classList.toggle("openandclose");
+
+  if (menu_list.classList.contains("openandclose")) {
+    menu_list_icon.src = "../assets/shared/icon-close.svg";
+    if (!menu_list.classList.contains("home"))
+      menu_list_icon.src = "../../assets/shared/icon-close.svg";
+  } else {
+    menu_list_icon.src = "../assets/shared/icon-hamburger.svg";
+    if (!menu_list.classList.contains("home"))
+      menu_list_icon.src = "../../assets/shared/icon-hamburger.svg";
+  }
+};
+
+// pages's transport transition
+document.querySelectorAll(".transition-link").forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    document.querySelector(".page-transition-overlay").style.opacity = 1;
+    setTimeout(() => {
+      window.location.href = href;
+    }, 500);
+  });
+});
